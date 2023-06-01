76886f43e36b1c62452efbbedc28551bb9793bf3
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-06-01 12:02:57 +0800

    Add message send and add result code logic

--- app/src/main/java/com/suhun/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/suhun/qrcodescanner/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -95,14 +98,32 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     }
 
     public void addFun(View view){
-
+        String scanResult = scanResultText.getText().toString();
+        HashMap<String, String> result = new HashMap<>();
+        result.put(from[0], scanResult);
+        data.add(result);
+        simpleAdapter.notifyDataSetChanged();
+        mScannerView.resumeCameraPreview(this);
+        addBtn.setEnabled(false);
     }
     public void sendFun(View view){
-
+        String scanResult[] = scanResultText.getText().toString().split(":");
+        Uri uri = Uri.parse("smsto:"+scanResult[1]);
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+        intent.putExtra("sms_body", scanResult[2]);
+        startActivity(intent);
     }
 
     @Override
-    public void handleResult(Result result) {
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v(tag, rawResult.getText()); // Prints scan results
+        Log.v(tag, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+        scanResultText.setText(rawResult.getText());
+        addBtn.setEnabled(true);
+        sendBtn.setEnabled(true);
+        // If you would like to resume scanning, call this method below:
+//        mScannerView.resumeCameraPreview(this);
 
     }
 }
