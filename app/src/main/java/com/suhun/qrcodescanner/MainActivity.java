2c4a1d0c789397c0e2f2a45124462a7deedd41f5
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-06-01 11:11:48 +0800

    Add camera permission logic code

--- app/src/main/java/com/suhun/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/suhun/qrcodescanner/MainActivity.java
@@ -1,7 +1,11 @@
 package com.suhun.qrcodescanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,7 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED){
+            initZXingQRCodeView();
+
+        }else{
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 123);
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                initZXingQRCodeView();
+
+            }else{
+                finish();
+            }
+        }
+    }
+
+    private void initZXingQRCodeView(){
+
+    }
+
     public void addFun(View view){
 
     }
